d2f4bf929176c62e0ca9a6fe3b9f30670eccc9d2
Manoel Moura <leomnx8001@gmail.com>
2023-11-27 16:52:51 -0300

    perfil adicionado

---
+++ src/app/profile/styles.css.ts
@@ -0,0 +1,31 @@
+export const textInput = `
+    bg-[#f6f6f6] 
+    text-base 
+    w-[25em]
+    h-[3.75em] 
+    p-5 my-3
+    rounded-[20px] 
+    border-2 
+    border-solid 
+    border-[#e8e8e8]
+    outline-none
+    
+` 
+export const bgOffwhite = `
+    w-[10rem]
+    h-[5rem]
+    flex
+    justify-center
+    items-center
+    m-auto
+    box-border
+`
+export const dashboard =
+  'bg-white w-5/6 h-full mt-6 mx-auto rounded-xl grid justify-center text-center'
+
+export const img = `
+  block
+  max-sm:hidden
+  ml-5
+  
+  `
