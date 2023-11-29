b1ac1a948c424b2279eab38155ee96da2342fc70
Rodrigo Ferreira <rodrigo.wendrel@protonmail.com>
2023-11-29 18:13:29 -0300

    login e responsividade

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
 
-export const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST};
