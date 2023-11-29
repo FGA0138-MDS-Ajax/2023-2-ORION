1c4040d7a59392df65c91e3348a7988abafa4302
Rodrigo Ferreira <rodrigo.wendrel@protonmail.com>
2023-11-29 18:25:49 -0300

    corrigindo o auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,3 @@
-import NextAuth from "next-auth"
-// import { Account, User as AuthUser } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import User from "@/models/User"
 import connect from "@/lib/mongodb"
