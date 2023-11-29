95809913990f8be83098990a1f7c3ee1dcfbeb8d
Rodrigo Ferreira <rodrigo.wendrel@protonmail.com>
2023-11-29 18:43:24 -0300

    corrigindo o auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,5 @@
 import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth"
-import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    return await NextAuth(req, res, authOptions);
-}
-
-
-
-
-// export { handler as GET, handler as POST };
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST };
