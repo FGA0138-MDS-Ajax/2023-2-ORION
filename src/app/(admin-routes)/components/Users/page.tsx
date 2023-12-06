b0096dc2a87e5a5065ad3eb7435a8d87c58b7cb1
Rodrigo Ferreira <rodrigo.wendrel@protonmail.com>
2023-12-06 08:53:55 -0300

    correções 2

--- src/app/(admin-routes)/components/Users/page.tsx
+++
@@ -1,9 +0,0 @@
- const Users = () => {
-    return (
-        <div>
-            <h1>Eventsdfdfd</h1>
-        </div>
-    )
-
-}
-export default Users
--- src/app/admin/login/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react"
-
-
-export default async function Login({ children }: { children: ReactNode }) {
-
-    return <>{ children }</>   
-}
